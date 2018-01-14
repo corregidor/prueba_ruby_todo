593d60740d1587c6aaf1dd44086a5435878e931a
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2018-01-14 15:52:05 -0300

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'todos/new'
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit'
 
   root 'todos#index'
 end
