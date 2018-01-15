451702594fbd72ef41b0d1fa4e8de49abdb1d525
sebastian fuenza rails <sebastian.fuenza@gmail.com>
2018-01-15 00:15:53 -0300

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'todos', to: 'todos#index'
-  get 'todos/new'
-  post 'todos', to: 'todos#create'
-  get 'todos/:id', to: 'todos#show', as: 'todo'
-  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
-  patch 'todos/:id', to: 'todos#update'
-  delete 'todos/:id', to: 'todos#destroy'
-
+  resources :todos
+  # get 'todos', to: 'todos#index'
+  # get 'todos/new'
+  # post 'todos', to: 'todos#create'
+  # get 'todos/:id', to: 'todos#show', as: 'todo'
+  # get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  # patch 'todos/:id', to: 'todos#update'
+  # delete 'todos/:id', to: 'todos#destroy'
+  #
   root 'todos#index'
 end
